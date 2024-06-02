52f8461310c9972643ce390d67c0a70726201d18
LaarouchiOussama <laarouchioussama@gmail.com>
2024-06-02 12:00:04 +0100

    Creating Student Model

--- src/models/student.js
+++ src/models/student.js
@@ -1,2 +1,10 @@
-
 // Create And Export Student Schema (Model) data => name, age, gender
+const mongoose = require('mongoose');
+
+const studentSchema = new mongoose.Schema({
+    name: String,
+    age: Number,
+    gender: String
+});
+
+module.exports = mongoose.model('Student', studentSchema);;
