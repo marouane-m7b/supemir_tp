ee7f028611307ce1d934de4403057de3070e803a
marouane-m7b <marouane.ma7boub@gmail.com>
2024-06-02 12:42:48 +0100

    fix errors

--- src/app.js
+++ src/app.js
@@ -5,22 +5,19 @@ const mongoose = require("mongoose");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use("/", routes);
+
 // Start Connect to MongoDB with error handling
 mongoose
   .connect(
-    ("mongodb://localhost:27017/school",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
+    ("mongodb://localhost:27017/school")
   )
   .then(() => console.log("Connection to MongoDB successful!"))
   .catch((err) => console.log(err));
 // End Connect to MongoDB
 
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use("/", routes);
 
 // Start Listen on port 3000
 app.listen(3000, () => console.log("Listening on port 3000"));
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,20 +1,31 @@
 const express = require("express");
 const router = express.Router();
+const path = require("path");
+const Student = require("../models/student");
 
 // Start route to get html file
 router.get("/", (req, res) => {
-  res.sendFile(__dirname + "/public/index.html");
+  res.sendFile(path.join(__dirname, "../../public/index.html"));
 })
 // End route to get html file
 
 // Start route to submit form data
 router.post("/submit", (req, res) => {
+  console.log(req.body);
   const name = req.body.name;
   const age = parseInt(req.body.age);
   const gender = req.body.gender;
 
-  if(!name || !age || !gender) {
-    return res.status(400).send("Nom invalide, veuillez renseigner tous les champs");
+  if(!name) {
+    return res.status(400).send("Nom invalide, veuillez renseigner un nom");
+  }
+
+  if(!age) {  
+    return res.status(400).send("Age invalide, veuillez renseigner un age");
+  }
+
+  if(!gender) {
+    return res.status(400).send("Genre invalide, veuillez renseigner un genre");
   }
 
   if(age > 15 || age < 12) {
@@ -23,6 +34,19 @@ router.post("/submit", (req, res) => {
 
   if(gender.includes("homme") || gender.includes("femme")) {
     return res.status(400).send("Genre invalide, veuillez renseigner un genre valide");
+  }
+
+  const student = new Student({
+    name: name,
+    age: age,
+    gender: gender
+  });
+
+  try {
+    student.save();
+    res.send("Enregistrement reussi");
+  } catch(err) {
+    res.status(500).send(err);
   }
 
 })
